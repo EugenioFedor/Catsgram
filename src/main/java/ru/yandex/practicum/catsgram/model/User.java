5bf6b7bbf12a5cab116cd2de455d07a6b9934428
Evgeny <bntk96lg@mail.ru>
2025-12-13 12:15:22 +0500

    Initial commit

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.catsgram.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.Instant;
+
+@EqualsAndHashCode(of = { "email" })
+public class User {
+    Long id;
+    String username;
+    String email;
+    String password;
+    Instant registrationDate;
+}
